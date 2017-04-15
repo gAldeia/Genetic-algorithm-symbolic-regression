5556e55c5d134647046a66e5b132248852def193
gAldeia <guilherme.aldeia@aluno.ufabc.edu.br>
2017-04-14 21:17:14 -0300

    Update main.cpp
    Editado para funcionar com as mudanças no binaryTree

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-const int pop_size = 5;
+const int pop_size = 20;
 const int pop_deepth = 10;
 
 const double mutation_rate = 0.01;
@@ -15,7 +15,12 @@ int main() {
 	population *pop = new population(pop_size);
 
 	pop->print_pop();
-
+	
+	for (int i=0; i<pop_size; i++) {
+		cout << "resultado da operação do individuo " << i 
+						<< ": " <<  pop->pop[i]->recupera_valor() << endl ;
+	}
+	
   	delete pop;
 		
 	return 0;
