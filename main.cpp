0617cb699a2efc0c3f23ff29b97c0bd75bcc36c6
Guilherme Aldeia <guilherme.aldeia@aluno.ufabc.edu.br>
2017-05-10 19:48:24 -0300

    Update main.cpp
    Algoritmo já está funcional

--- main.cpp
+++ main.cpp
@@ -6,60 +6,46 @@
 
 using namespace std;
 
-const int pop_size = 25;
-const int pop_deepth = 4;
 
-const double mutation_rate = 20;
 
-const double target_values[] = {0.0, 2.0, 4.0, 6.0, 8.0, 10.0, 12.0};
-const int    target_values_size = 7;
+const int pop_size = 150;
+const int pop_deepth = 5;
+
+double mutation_rate = 20;
+double crossover_rate = 50;
+
+double target_values[] = {1.0, 4.0, 16.0, 64.0, 256.0};
+int target_values_size = 5;
+
+
 
 int main() {
 
 	population *pop = new population(pop_size, pop_deepth);
+	node *aux;
+	double melhor;
 
-	cout << "pop:" << endl;
-	pop->print_pop();
-	cout << endl;
 
-	pop->set_backup();
-	pop->mutation(mutation_rate);
-	
-	cout << "backup: " << endl;
-	pop->print_backup();
-	cout << endl;
-	
-	cout << "pop apos mutating: " << endl;
-	pop->print_pop();
-	cout << endl;
+	for (int i=0; i<1000; i++) {
+		aux = pop->melhor_mse(target_values, target_values_size);
 
-	for (int i=0; i<pop_size; i++)
-		cout <<"MSE " << i << ": " << pop->pop[i]->recupera_mse(target_values, target_values_size) << endl;
-		
-	cout << endl;
-	
-	node aux = pop->fitness(target_values, target_values_size);
-	
-	cout << "melhor eq selecionada pelo fitness: ";
-	aux.print_node();
-	cout << endl << endl;
-	
-	node *copia1, *copia2;
-	
-	for (int i=0; i< pop_size-1; i++) {
-		cout << "pais: " << endl;
-		copia1 = pop->pop[i]->recupera_copia();
-		copia2 = pop->pop[i+1]->recupera_copia();
-		cout << "\t"; copia1->print_node(); cout << endl;
-		cout << "\t"; copia2->print_node(); cout << endl;
-	
-		cout << "filhos: " << endl;
-		copia1->crossover_with(copia2, 25);
-		cout << "\t"; copia1->print_node(); cout << endl;
-		cout << "\t"; copia2->print_node(); cout << endl << endl;
+		melhor = aux->recupera_mse(target_values, target_values_size);
+
+		if (melhor==0){
+			cout << "ACHOU: "; aux->print_node();
+			cout << "; na geração " << i << endl;
+			break;
+		}
+
+		pop->next_gen(mutation_rate, target_values,
+					  target_values_size, crossover_rate);
 	}
-	
-  	delete pop;
-		
+
+	pop->melhor_mse(target_values, target_values_size)->print_node();
+	cout << endl;
+	pop->print_pop();
+
+	delete pop;
+
 	return 0;
 }
