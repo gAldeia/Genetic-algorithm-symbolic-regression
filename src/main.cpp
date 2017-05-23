d59989e748f659bee74906592e1901011c573b9a
Guilherme Aldeia <guilherme.aldeia@aluno.ufabc.edu.br>
2017-05-23 15:42:39 -0300

    Atualizado
    Arquivo atualizado para funcionar de acordo com as atualizações no BinaryTree.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,42 +8,42 @@ using namespace std;
 
 
 
-const int pop_size = 150;
-const int pop_deepth = 5;
+const int pop_size = 500;
+const int pop_depth = 10;
 
-double mutation_rate = 20;
-double crossover_rate = 50;
+double mutation_rate = 10;
+double crossover_rate = 40;
 
-double target_values[] = {1.0, 4.0, 16.0, 64.0, 256.0};
+double target_values[] = {1.0, 2.0, 4.0, 8.0, 16.0};
 int target_values_size = 5;
 
 
 
 int main() {
 
-	population *pop = new population(pop_size, pop_deepth);
+	population *pop = new population(pop_size, pop_depth);
 	node *aux;
 	double melhor;
 
 
-	for (int i=0; i<1000; i++) {
-		aux = pop->melhor_mse(target_values, target_values_size);
+	for (int i=0; i<100; i++) {
+		aux = pop->get_best_mse(target_values, target_values_size);
 
-		melhor = aux->recupera_mse(target_values, target_values_size);
+		melhor = aux->get_mse(target_values, target_values_size);
 
 		if (melhor==0){
-			cout << "ACHOU: "; aux->print_node();
+			cout << "ACHOU: "; aux->print_node_d();
 			cout << "; na geração " << i << endl;
 			break;
 		}
 
-		pop->next_gen(mutation_rate, target_values,
+		pop->generate_next_gen(mutation_rate, target_values,
 					  target_values_size, crossover_rate);
 	}
 
-	pop->melhor_mse(target_values, target_values_size)->print_node();
+	pop->get_best_mse(target_values, target_values_size)->print_node_d();
 	cout << endl;
-	pop->print_pop();
+	pop->print_pop_d();
 
 	delete pop;
 
