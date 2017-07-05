6b8a66a17e0f05e69491c2dbe73093daefa8b9c0
Guilherme Aldeia <guilherme.aldeia@aluno.ufabc.edu.br>
2017-07-05 17:34:39 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <cstdlib>
 #include <vector>
-#include <string>
 
 
 using namespace std;
@@ -11,17 +10,17 @@ using namespace std;
 //GUARDAR FUNÇÕES ÚTEIS IMPLEMENTADAS POR NÓS (NÓS != NODES)------------------//
 class Useful{
 
-    //se não me engano, static são métodos que podem ser utiliados sem
-    //instanciar uma classe.
-
     public:
         static double func1_solver(int function, double A, double B);
         static double func2_solver(int function, double A, double B);
 
         static double div(double A, double B);
         static double sum(double A, double B);
+        static double sub(double A, double B);
+        static double mut(double A, double B);
 
-        //e assim vai...
+        static double   sen(double A);
+        static double cosen(double A);
 };
 
 
@@ -30,11 +29,11 @@ class Node{
 
     private:
         enum {
-            var = 0,
-            cte = 1,
-            fun1 = 2,
-            fun2 = 3
-        } Definitions;
+            var,
+            cte,
+            fun1,
+            fun2
+        } Definition;
 
         union {
             int idX;        //var
@@ -73,137 +72,21 @@ class Specimen : public Node{
 };
 
 
-//----------------------------------------------------------------------------//
-int main(){
+//CLASSE QUE FUNCIONA COMO O MEIO AMBIENTE PARA DARWIN------------------------//
+class Environment{
 
-    return 0;
-}
-
-/*
-
-//----------------------------------------------------------------------------//
-// Ideias para melhorar a implementação do código principal.
-//----------------------------------------------------------------------------//
-#include <iostream>
-#include <cstdlib>
-#include <vector>
-#include <string>
-
-
-using namespace std;
-
-
-//----------------------------------------------------------------------------//
-class Number {
     private:
-        //vetor que guarda as possíveis variáveis, sorteadas no construtor
-        vector<char> variables {'x', 'y'};
-
-        //diz se este número deverá ser tratado como constante ou como variável.
-        //é constante, então após inicialização não pode ser alterada
-        const bool definedAsConst;
-
-        //armazena o valor numérico number.
-        double xValue;
-        double yValue;
-
-        //método que sorteia o valor da constante, caso necessário.
-        double sort_const(double start, double finale);
+        Specimen *pop;
+        int popSize;
 
     public:
-        //construtor void, que deve fazer um tratamento simples caso nada seja
-        //especificado
-        Number();
+        Environment();
+        Environment(int popSize);
+        ~Environment();
 
-        //construtor que de acordo com a entrada (V ou F) prepara o Number para
-        //atuar como uma constante ou uma variável. é importante o construtor
-        //definir a definedAsConst. é importante saber se será 2 variáveis ou
-        //não para não criar Ys na árvore caso não seja, pois pode criar erros.
-        Number(bool definedAsConst, bool isTwoVariables);
-
-        //destrutor
-        ~Number();
-
-        //sobrecarga do operador '=', para receber o valor de x, caso seja uma
-        //variável (uma variável tem que ser apta a mudanças).
-        //é preciso que este método faça um tratamento interno para não acabar
-        //modificando o valor caso tenha sido declarado como uma constante.
-        //v deve ser um vetor de no máximo 2 valores, contendo ou {x} ou {x, y}.
-        void operator=(vector<double> *v);
-};
-
-//----------------------------------------------------------------------------//
-class Function{
-    private:
-        //conjunto de funções que pode assumir. (operar com string deixa mais
-        //legivel que com char)
-        vector<string> function_pool {"func1", "func2", "etc"};
-
-        //bool para saber se opera com 1 ou 2 variáveis.
-        const bool isTwoVariables;
-
-        //string que guarda a função do nó
-        string function;
-
-    public:
-        //método que resolve a função, com polimorfismo para funcionar com 1 ou
-        //2 variáveis. O solve deve saber operar de acordo com: o número de va-
-        //riáveis passadas e a função salva em function.
-        double solve(double x);
-        double solve(double x, double y);
-    
-        //construtor void, que deve fazer um tratamento simples caso nada seja
-        //especificado
-        Function();
-
-        //o construtor sorteia a função que a classe irá assumir, sempre consi-
-        //derando se é ou não um nó de duas variáveis.
-        Function(bool isTwoVariables);
-
-        ~Function();
-};
-
-//----------------------------------------------------------------------------//
-class Node {
-    private:
-        //ponteiros para o número e tipo do nó. O bom de usar ponteiros aqui É
-        //que podemos criar o number e function dentro do construtor do Node.
-        //o construtor do Node podia sortear se vai ser uma função ou um número
-        //e com isso criar um ponteiro para um dos tipos e outro como null. aí,
-        //as funções da classe node fazem uma verificação simples para ver
-        //o que o nó é e operam com as funções do seu tipo.
-        Number *value = NULL;
-        Function *func = NULL;
-
-        //variável que guarda sua profundidade.
-        int depth;
-
-        //ponteiros para seus filhos.
-        Node *left = NULL;
-        Node *right = NULL;
-
-    public:
-        
-        //construtor void, que deve fazer um tratamento simples caso nada seja
-        //especificado
-        Node();
-
-        //é importante o nó saber se vai ser de duas variáveis ou não, pois ele
-        //que irá chamar os construtores de Number e Function. Tudo tem que ser
-        //compatível, então ele tem que criar as classes com sabedoria.
-        Node(int depth, bool isTwoVariables);
-
-        ~Node();
-
-        //aqui é preciso incorporar alguns métodos já existentes no arquivo
-        //principal ou então modificá-los para funcionar com o novo value.
-
-        //essas funções são necessárias para Number e Function pois os membros
-        //de Node são privados.
-        Number *get_NumberPt();
-        Function *get_FunctionPt();
-        Node *get_leftPt();
-        Node *get_rightPt();
+        void apply_crossover();
+        void apply_mutation();
+        void apply_fitness();
 };
 
 
@@ -212,5 +95,3 @@ int main(){
 
     return 0;
 }
-
-*/
