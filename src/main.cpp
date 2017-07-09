a4581e42c1dbec14cc56aa7d7892f2fb4cbec5af
Guilherme Aldeia <guilherme.aldeia@aluno.ufabc.edu.br>
2017-07-09 14:14:44 -0300

    1.1 Atualizado para testar as novas implementações

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,27 @@
 #include "utils.hpp"
 #include "node.hpp"
-#include <iostream>
 
+#include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 
 int main(){
 
-    double r = utils::add(1,1);
+    srand(time(NULL));
 
+
+    for(int i=0; i<10; i++){
+    Node *root = new Node();
+
+    cout << "A eq. gerada do node é (torça para ser pequena): ";
+    root->print_node_d();
+    cout << endl;
+
+    vector<double> x = {double (random()%10) };
+
+    cout << "Para x = " << x[0] << ", O valor da eq. do node é: " << root->eval(x) << endl << endl;
+    }
     return 0;
 }
