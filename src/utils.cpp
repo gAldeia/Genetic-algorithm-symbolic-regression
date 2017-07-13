d34b8d0b724ab8c2539a1d7474fcd8d0680d7382
Guilherme Aldeia <guilherme.aldeia@aluno.ufabc.edu.br>
2017-07-12 21:55:14 -0300

    1.1.2 Remoção de espaços em branco

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,82 +1,80 @@
-#include "utils.hpp"
-
+//utils.cpp
 #include <cmath>
 #include <iostream>
+#include "utils.hpp"
+
 
 namespace utils{
 
-    // Função Soma
+
+    //=========================FUNÇÕES BINÁRIAS===========================//
+
+
+    //Função Soma
     double uAdd(double v1, double v2) {
 
         return (v1 + v2);
     }
 
-
-    // Função Subtração
+    //Função Subtração
     double uSub(double v1, double v2) {
 
         return (v1 - v2);
     }
 
-
-    // Função Multiplicação
+    //Função Multiplicação
     double uMult(double v1, double v2) {
 
         return (v1 * v2);
     }
 
-
-    // Função Divisão
+    //Função Divisão
     double uDiv(double v1, double v2) {
 
-        if (v2 == 0) {
+        if (v2 == 0)
             return 0;   //Padrão
-        }
-        else {
+        else
             return (v1 / v2);
-        } 
     }
 
-    // Função Potência
+    //Função Potência
     double uPow(double v1, double v2) {
 
-        if ((v1 == 0)&&(v2 == 0)) {
+        if ((v1 == 0)&&(v2 == 0))
             return 0;   //Padrão - Equivalente matemático a 0/0
-        }
-        else {
-            return pow(v1, v2);
-        }    
+        else
+            return pow(v1, v2); 
     }
 
-    // Função Exponencial (e^v)
+
+    //==========================FUNÇÕES UNÁRIAS===========================//
+
+
+    //Função Exponencial (e^v)
     double uExp(double v) {
 
         return (exp(v));
     }
 
-    // Função Raiz Quadrada
+    //Função Raiz Quadrada
     double uSqrt(double v) {
 
-        if (v < 0) {
+        if (v < 0)
             return 0;
-        }
-        else {
+        else
             return (sqrt(v));
-        }
     }
 
-    // Função Logaritmo Neperiano
+    //Função Logaritmo Neperiano
     double uLn(double v) {
 
-        if (v > 0) {
+        if (v > 0)
             return (log(v));
-        }
-        else {
+        else
             return 0;
-        }
     }
 
-    // Converter radianos em graus para poder utilizar as funções trigonométricas
+    //Converter radianos em graus para poder utilizar as funções trigonométricas
     double rad_to_degrees(double v) {
         
         double V = ((v*180)/(M_PI));
@@ -86,78 +84,72 @@ namespace utils{
         return V;
     }
 
-    // Função Seno
+    //Função Seno
     double uSin(double v) {
 
         return (sin(rad_to_degrees(v)));
     }
 
-
-    // Função Cosseno
+    //Função Cosseno
     double uCos(double v) {
 
         return (cos(rad_to_degrees(v)));
     }
 
-
-    // Função Tangente
+    //Função Tangente
     double uTan(double v) {
 
         double V = rad_to_degrees(v);
-        if ((V == 90)||(V == 270)) {
+
+        if ((V == 90)||(V == 270))
             return 0;
-        }
-        else {
+        else
             return (tan(V));
-        }
     }
 
+
+    //=====================SOLUCIONADOR DE EQUAÇÕES=======================//
+
+
+    //Solucionador de equações de aridade 1
     double func1_solver(int F_id, double v) {
         
         switch (F_id) {
             case LN:
                 return uLn(v);
-                break;
             case EXP:
                 return uExp(v);
-                break;
             case SQRT:
                 return uSqrt(v);
-                break;
             case SIN:
                 return uSin(v);
-                break;
             case COS:
                 return uCos(v);
-                break;
             case TAN:
                 return tan(v);
-                break;
             default:
                 std::cout << "ERRO FUNC1 SOLVER" << std::endl;
+                return -1.0;
         }
     }
 
+    //Solucionador de equações de aridaade 2
     double func2_solver(int F_id, double v1, double v2) {
         
         switch (F_id) {
             case ADD:
                 return uAdd(v1, v2);
-                break;
             case SUB:
                 return uSub(v1, v2);
-                break;
             case MULT:
                 return uMult(v1, v2);
-                break;
             case DIV:
                 return uDiv(v1, v2);
-                break;
             case POW:
                 return uPow(v1, v2);
-                break;
             default:
                 std::cout << "ERRO FUNC2 SOLVER" << std::endl;
+                return -1.0;
         }
     }
 }
