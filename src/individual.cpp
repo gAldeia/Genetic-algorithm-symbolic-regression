ee21d6ecd0f63fedc562672ba8bb816c90fbbf9f
Guilherme Aldeia <guilherme.aldeia@aluno.ufabc.edu.br>
2017-07-15 18:57:06 -0300

    1.1.1 Apagado implementação ruim de mutation

--- src/individual.cpp
+++ src/individual.cpp
@@ -20,29 +20,7 @@ Node *Individual::crossover(double crossover_rate, Individual parent){
 }
 
 void Individual::mutation(double mutation_rate){
-
-    double rate = mutation_rate*100;
-
-    if (random()%100<rate){
-        int new_tipo = random() %utils::SIZETYPE;
-
-        std::cout << "new tipo: " << new_tipo << std::endl;
-
-        switch (new_tipo){
-            case utils::VAR:
-                expression->set_C(utils::VAR, random()%2);
-                break;
-            case utils::CTE:
-                expression->set_C(utils::CTE, random()%10);
-                break;
-            case utils::FUN1:
-                expression->set_C(utils::FUN1, random()%utils::SIZEFUNC1);
-                break;
-            case utils::FUN2:
-                expression->set_C(utils::FUN2, random()%utils::SIZEFUNC2);
-                break;
-        }
-    }
+    //falta implementar aqui
 }
 
 double Individual::fitness(std::vector<utils::DataPoint> points){
