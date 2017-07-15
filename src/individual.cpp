545a33fde99f9642636d3532a91862a3c473c87f
Guilherme Aldeia <guilherme.aldeia@aluno.ufabc.edu.br>
2017-07-15 18:56:12 -0300

    1.1 Implementado fitness
    -agora calcula o fitness para um vetor de pontos

--- src/individual.cpp
+++ src/individual.cpp
@@ -1,6 +1,7 @@
 //individual.cpp
 #include <vector>
 #include <cstdlib>
+#include <iostream>
 
 #include "individual.hpp"
 #include "utils.hpp"
@@ -23,29 +24,33 @@ void Individual::mutation(double mutation_rate){
     double rate = mutation_rate*100;
 
     if (random()%100<rate){
-        //aqui eu sorteio um novo tipo de nó, e com isso um novo valor de C
-        //também
-        int tipo_anterior = expression->get_type();
         int new_tipo = random() %utils::SIZETYPE;
 
-        if (tipo_anterior==new_tipo) {
-            //caso continue igual, sorteia outro valor
-        }
-        else {
-            //caso mude, verifica se é func1 ou 2 e toma as devidas ações
-            //ou seja -> ou cria um novo filho ou deleta um para que a árvore 
-            //não tenha pedaços que não sejam utilizados ou tenha pedaços
-            //faltando.
+        std::cout << "new tipo: " << new_tipo << std::endl;
+
+        switch (new_tipo){
+            case utils::VAR:
+                expression->set_C(utils::VAR, random()%2);
+                break;
+            case utils::CTE:
+                expression->set_C(utils::CTE, random()%10);
+                break;
+            case utils::FUN1:
+                expression->set_C(utils::FUN1, random()%utils::SIZEFUNC1);
+                break;
+            case utils::FUN2:
+                expression->set_C(utils::FUN2, random()%utils::SIZEFUNC2);
+                break;
         }
     }
 }
 
-double Individual::fitness(vector< vector<double> > points){
+double Individual::fitness(std::vector<utils::DataPoint> points){
    
     double mse = 0.0;
     
     for(int i=0; i<points.size(); i++){
-        mse += utils::uPow((expression->eval(points[0][i])-points[1][i] ), 2);
+        mse += utils::uPow((expression->eval(points[i])-points[i].y ), 2);
     }
     
     this->mse_value = utils::uSqrt(mse/(double)points.size());
