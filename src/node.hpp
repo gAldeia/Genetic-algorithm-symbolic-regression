7a3ce4c02e708a78c1e5e05e9fbc0cc22ef0a7b0
Guilherme Aldeia <guilherme.aldeia@aluno.ufabc.edu.br>
2017-07-16 20:39:24 -0300

    1.5 Corrigidas as novas assinaturas
    -novas assinaturas dos métodos
    -novos métodos para melhorar a modularização do código
    -um pouco mais de comentários

--- src/node.hpp
+++ src/node.hpp
@@ -2,40 +2,46 @@
 #ifndef _NODE_H
 #define _NODE_H
 
-
 #include <vector>
-
 #include "utils.hpp"
 
-class Node{
 
+using namespace utils;
+
+
+class Node{
     private:
         union Content{
-            double idX;     //var
-            double value;   //const
-            int function;   //func1 ou func2
+            double idX;      //guarda o índice do var(depende de numberOfXs)
+            double value;    //constante sorteada de 0-9
+            int    function; //func1 ou func2
         };
 
-        Content C;
-        int tipo; //recebe o índice do content
+        Content C;           //union que guarda o valor do nó
+        
+        int tipo;            //recebe o índice do content
+        int numberOfXs;      //número de xn que a árvore pode criar
+        int maxDepth;        //maior profundidade que sua subárvore pode ter
 
         Node *left;
         Node *right;
 
+        void makeThisVar();
+        void makeThisCte();
+        void makeThisFunc1(bool grow);
+        void makeThisFunc2(bool grow);
+
+        void growStyle();
+        void fullStyle();        
+
     public:
-        Node(bool copy = false, bool grow = true, int maxDepth = 3, int numberOfXs = 1);
+        Node(bool grow = true, int maxDepth = 0, int numberOfXs = 1);
         ~Node();
 
-        void growStyle(int maxDepth, int numberOfXs);
-        void fullStyle(int maxDepth, int numberOfXs);
-        void print_node_d();
-
-        Node *get_copy();
-        int get_type();
-
-        void changeNumberOfSubtreesTo(int quantity);
-
-        double eval(utils::DataPoint p);
+        Node * get_copy();
+        int    get_type();
+        void   print_node_d();
+        double eval(DataPoint p);
 };
 
 #endif
