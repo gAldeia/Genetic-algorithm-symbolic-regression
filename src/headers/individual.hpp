3314bf46f5ef15458208c1501b7066645be7ce06
Guilherme Aldeia <guilherme.aldeia@aluno.ufabc.edu.br>
2017-08-01 16:12:46 -0300

    1.4 Novos métodos

--- src/headers/individual.hpp
+++ src/headers/individual.hpp
@@ -11,7 +11,6 @@ using namespace std;
 
 
 class Individual{
-    
     private:
         double mse_value;
 
@@ -21,12 +20,13 @@ class Individual{
         Individual(bool grow = false, int maxDepth = 3, int numberOfXs = 1);
         ~Individual();
 
-        Node *crossover(double crossover_rate, Individual parent);
-        void mutation(double mutation_rate);
-        double fitness(vector<utils::DataPoint> points);
+        Individual *crossover(double crossover_rate, Individual *parent);
+        void        mutation(double mutation_rate);
+        double      fitness(vector<utils::DataPoint> points);
+        Individual *get_copy();
 
-        void print_expression_d();
-        void print_fitness_d();
+        void        print_expression_d();  //métodos de debug
+        double      get_fitness();
 };
 
 #endif
